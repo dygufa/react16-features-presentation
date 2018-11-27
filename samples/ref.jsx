f1e6b8c4ee5ea146fcece574c2491d1b30c40432
Victor Magalhães <victorhfmag@gmail.com>
2018-11-27 13:42:41 -0300

    Iterate on ref example

---
+++ samples/ref.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const FancyInput = React.forwardRef((props, ref) => (
+    <input {...props} ref={ref} className="fancy-input" />
+));
+
+class App extends React.Component {
+    inputRef = React.createRef();
+
+    componentDidMount() {
+        inputRef.current.focus();
+    }
+
+    render() {
+        return (
+            <form>
+                <label>Digite seu nome de usuário</label>
+                <FancyInput
+                    ref={this.inputRef}
+                    value={this.state.username}
+                    onChange={ev =>
+                        this.setState({ username: ev.target.value })
+                    }
+                />
+            </form>
+        );
+    }
+}
