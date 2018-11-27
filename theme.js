2daeb6da9220324adb662baf7589e285f036c3d4
Victor Magalhães <victorhfmag@gmail.com>
2018-11-27 14:47:25 -0300

    Enable syntax highlight in code fences

---
+++ theme.js
@@ -0,0 +1,9 @@
+import theme from "mdx-deck/themes";
+import okaidia from "react-syntax-highlighter/styles/prism/okaidia";
+
+export default {
+    ...theme,
+    prism: {
+        style: okaidia,
+    },
+};
