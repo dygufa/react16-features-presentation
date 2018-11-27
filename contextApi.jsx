d7d6977d849a903a0a88d700f6ff88e65e52cb00
Rodrigo Araujo <dygufa@gmail.com>
2018-11-27 12:56:51 -0300

    adding context api example

---
+++ contextApi.jsx
@@ -0,0 +1,23 @@
+const ThemeContext = React.createContext('light');
+
+class ThemeProvider extends React.Component {
+    state = { theme: 'light' };
+
+    render() {
+        return (
+            <ThemeContext.Provider value={this.state.theme}>
+                {this.props.children}
+            </ThemeContext.Provider>
+        );
+    }
+}
+
+class ThemedButton extends React.Component {
+    render() {
+        return (
+            <ThemeContext.Consumer>
+                {theme => <Button theme={theme} />}
+            </ThemeContext.Consumer>
+        );
+    }
+}
